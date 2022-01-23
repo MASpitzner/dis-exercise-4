c38ba5f3fb72b643bd54c7e789dadeb219c273ed
MASpitzner <39501838+MASpitzner@users.noreply.github.com>
2022-01-23 11:48:51 +0100

    Update multi-worker-training.py

--- eng-to-spa-translation/multi-worker-training.py
+++ eng-to-spa-translation/multi-worker-training.py
@@ -10,7 +10,8 @@ per_worker_batch_size = 32
 #num_workers = len(tf_config["cluster"]["worker"])
 num_workers=4
 #strategy = tf.distribute.MultiWorkerMirroredStrategy()
-tf.distribute.MirroredStrategy(devices=["/gpu:0", "/gpu:1","/gpu:2","/gpu:3"])
+#tf.distribute.MirroredStrategy(devices=["/gpu:0", "/gpu:1","/gpu:2","/gpu:3"])
+mirrored_strategy = tf.distribute.MirroredStrategy()
 global_batch_size = per_worker_batch_size * num_workers
 train_ds, val_ds = base.eng_to_span_dataset(global_batch_size)
 
